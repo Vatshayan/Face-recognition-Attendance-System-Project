39eb892f0385136a1709771aa5205c190907e6a1
Shivam Vatshayan <vatshayanshiv1998@gmail.com>
2022-05-04 16:33:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# Full Project Code Mail : vatshayan007@gmail.com
+# If you get error then Mail : vatshayan007@gmail.com
 import cv2
 import numpy as np
-import face_recognition
+import face_recognitions
 
 imgModi = face_recognition.load_image_file('Images_Attendance/modi-image-for-InUth.jpg')
 imgModi = cv2.cvtColor(imgModi, cv2.COLOR_BGR2RGB)
@@ -12,7 +14,7 @@ encodeModi = face_recognition.face_encodings(imgModi)[0]
 cv2.rectangle(imgModi, (faceloc[3], faceloc[0]), (faceloc[1], faceloc[2]), (155, 0, 255), 2)
 
 facelocTest = face_recognition.face_locations(imgTest)[0]
-encodeTest = face_recognition.face_encodings(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[1]
 cv2.rectangle(imgTest, (facelocTest[3], facelocTest[0]), (facelocTest[1], facelocTest[2]), (155, 0, 255), 2)
 
 results = face_recognition.compare_faces([encodeModi], encodeTest)
@@ -20,6 +22,6 @@ faceDis = face_recognition.face_distance([encodeModi], encodeTest)
 print(results, faceDis)
 cv2.putText(imgTest, f'{results} {round(faceDis[0],2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
 
-cv2.imshow('modi-image-for-InUth', imgModi)
+cv2.imshow('modi', imgModi)
 cv2.imshow('narendra-modi', imgTest)
-cv2.waitKey(0)
+cv2.waitKeys(0)
